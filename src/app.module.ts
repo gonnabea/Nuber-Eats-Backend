3dba44a762c6fcf2c7fe119034b9e83eb344a939
Choi Jiwon <winning1993@naver.com>
2020-10-29 06:45:10 +0900

    #2.4 Introducing ConfigService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,20 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { RestaurantsModule } from './restaurants/restaurants.module';
-import { join } from "path";
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ // .env 설정 for nest.js way
+      isGlobal: true, // 모든 경로에서 접근 가능하게
+      envFilePath: process.env.NODE_ENV === "dev" ? ".env.dev" : ".env.test"// dotenv 파일 경로
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: true // 그래프큐엘 스키마 파일 저장경로 (true일 시 따로 생성되지 않는 듯 함)
     }),
-    RestaurantsModule,
+    
     TypeOrmModule.forRoot({
       "type": "postgres",
       "host": "localhost",
@@ -20,7 +24,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       "database": "nuber-eats",
       "synchronize": true,
       "logging": true,
-    })
+    }),
+    RestaurantsModule,
   ],
   controllers: [],
   providers: [],
