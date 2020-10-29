3bf1e0164f38c2843f0c4ac9bb5ae6e6921b844d
Choi Jiwon <winning1993@naver.com>
2020-10-29 23:30:11 +0900

    #2.5 Configuring ConfigService

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,21 +9,21 @@ import { ConfigModule } from '@nestjs/config';
   imports: [
     ConfigModule.forRoot({ // .env 설정 for nest.js way
       isGlobal: true, // 모든 경로에서 접근 가능하게
-      envFilePath: process.env.NODE_ENV === "dev" ? ".env.dev" : ".env.test"// dotenv 파일 경로
+      envFilePath: process.env.NODE_ENV === "dev" ? ".env.dev" : ".env.test",// dotenv 파일 경로
+      ignoreEnvFile: process.env.NODE_ENV === "prod" // 배포할 때
     }),
     GraphQLModule.forRoot({
       autoSchemaFile: true // 그래프큐엘 스키마 파일 저장경로 (true일 시 따로 생성되지 않는 듯 함)
     }),
-    
     TypeOrmModule.forRoot({
-      "type": "postgres",
-      "host": "localhost",
-      "port": 5432,
-      "username": "Jiwon",
-      "password": "fk2qjf!!",
-      "database": "nuber-eats",
-      "synchronize": true,
-      "logging": true,
+      type: "postgres",
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: true,
+      logging: true,
     }),
     RestaurantsModule,
   ],
