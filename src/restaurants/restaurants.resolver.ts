e5c09d791dcaea0505d80d3a18306ec65c969238
Choi Jiwon <winning1993@naver.com>
2020-11-02 09:37:12 +0900

    #3.7 Update Restaurant part One

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,6 +1,7 @@
 
 import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
 import { CreateRestaurantDto } from "./dtos/create-restaurant.dto";
+import { UpdateRestaurantDto } from "./dtos/update-restaurant.dto";
 import { Restaurant } from "./entities/restaurant.entity";
 import { RestaurantService } from "./restaurants.service";
 
@@ -26,5 +27,12 @@ export class RestaurantResolver {
            console.log(e)
            return false
        }
+    }
+
+    @Mutation(returns => Boolean)
+    async updateRestaurant(
+        @Args('input') updateRestaurantDto: UpdateRestaurantDto
+    ){
+        return true
     }
 }
