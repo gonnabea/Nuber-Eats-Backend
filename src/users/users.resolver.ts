ca1c3466c0c65e2567fa2fc5325a40998a45ef23
Choi Jiwon <winning1993@naver.com>
2020-11-11 10:30:34 +0900

    #4.6 An Alternative Error

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -18,20 +18,15 @@ export class UsersResolver {
     @Mutation(returns => CreateAccountOutput)
     async createAccount(@Args("input") createAccountInput: CreateAccountInput): Promise<CreateAccountOutput> {
         try {
-            const error = await this.usersService.createAccount(createAccountInput)
-            if(error){
-                return {
-                    ok: false,
-                    error
-                }
-            }
+            const {ok, error} = await this.usersService.createAccount(createAccountInput)
             return {
-                ok: true
+                ok,
+                error
             }
         } catch(error) {
             return {
+                ok: false,
                 error,
-                ok: false
             }
         }
     }
