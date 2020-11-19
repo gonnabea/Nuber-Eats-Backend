0dde5a672ef1fef1c7bf970c1a3bb61b2791b13c
Choi Jiwon <winning1993@naver.com>
2020-11-19 15:37:57 +0900

    #4.8 Log In part One

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -3,6 +3,7 @@ import { CoreEntity } from "src/common/entities/core.entity";
 import { BeforeInsert, Column, Entity } from "typeorm";
 import * as bcrypt from "bcrypt"
 import { InternalServerErrorException } from "@nestjs/common";
+import { IsEmail, IsEnum } from "class-validator";
 
 enum UserRole {
     Client,
@@ -19,14 +20,16 @@ export class User extends CoreEntity {
 
     @Column()
     @Field(type => String)
+    @IsEmail()
     email: string;
 
     @Column()
     @Field(type => String)
     password: string;
-
+    
     @Column({ type: 'enum', enum: UserRole })
     @Field(type => UserRole)
+    @IsEnum(UserRole)
     role: UserRole;
 
     @BeforeInsert()
