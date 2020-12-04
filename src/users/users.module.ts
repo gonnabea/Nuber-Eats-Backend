646cbdfa5aebdedd266851b1931d96e891c4fcf8
Choi Jiwon <winning1993@naver.com>
2020-12-04 13:32:31 +0900

    #7.0 Setting Up Tests

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
-import { UsersService } from './users.service';
+import { UserService } from './users.service';
 import { UsersResolver } from "./users.resolver"
 import { Verification } from './entities/verification.entity';
 
 @Module({
     imports: [TypeOrmModule.forFeature([User, Verification])],
-    providers: [UsersResolver, UsersService],
-    exports: [UsersService]
+    providers: [UsersResolver, UserService],
+    exports: [UserService]
 })
 export class UsersModule {}
 
