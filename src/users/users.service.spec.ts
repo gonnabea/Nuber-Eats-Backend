d75bfaf0c461b7e0902ef68bbb76f9cb9d112b6b
Choi Jiwon <winning1993@naver.com>
2020-12-08 10:11:15 +0900

    #7.7 login Test part One

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -108,8 +108,32 @@ describe("UserService", () => {
         expect(result).toEqual({ok:true})
     })
  
+    it('should fail on exception', async() => {
+        usersRepository.findOne.mockRejectedValue(new Error("에러"))
+        const result = await service.createAccount(createAccountArgs)
+        expect(result).toEqual({ ok: false, error: "Couldn't create account"})
+        
+    })
    })
 
+    describe("login", () => {
+        const loginArgs = {
+            email: "test@email.com",
+            password: "test"
+        }
+        it("should fail if user does not exist", async () => {
+            usersRepository.findOne.mockResolvedValue(null);
+
+            const result = await service.login(loginArgs)
+
+            expect(usersRepository.findOne).toHaveBeenCalledTimes(1)
+            expect(usersRepository.findOne).toHaveBeenCalledWith(expect.any(Object), expect.any(Object))
+            expect(result).toEqual({
+                ok: false,
+                error: "User not found"
+            })
+        })
+    })
     it.todo('login')
     it.todo('findById')
     it.todo('editProfile')
