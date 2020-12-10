b387c6e67786aa994bbbc9c15918a3360f4768bb
Choi Jiwon <winning1993@naver.com>
2020-12-10 11:55:20 +0900

    #8.1 JWT Sign Test

--- src/jwt/jwt.service.spec.ts
+++ src/jwt/jwt.service.spec.ts
@@ -1,6 +1,13 @@
 import { Test } from "@nestjs/testing"
 import { CONFIG_OPTIONS } from "src/common/common.constants"
 import { JwtService } from "./jwt.service"
+import * as jwt from "jsonwebtoken"
+
+jest.mock('jsonwebtoken', () => {
+    return {
+        sign: jest.fn(() => "TOKEN")
+    }
+})
 
 const TEST_KEY = "testKey"
 
@@ -18,6 +25,18 @@ describe("JwtService", () => {
     it("should be defined", () => {
         expect(service).toBeDefined()
     })
-    it.todo("sign")
-    it.todo("verify")
+describe("sign", () => {
+    it("should return a sign token", () => {
+        const ID = 1
+        const token = service.sign(ID)
+        expect(typeof token).toBe("string")
+        expect(jwt.sign).toHaveBeenCalledTimes(ID)
+        expect(jwt.sign).toHaveBeenCalledWith({id:ID},TEST_KEY)
+    })
+})
+describe("verify", () => {
+    if("should return the decoded token", () => {
+
+    })
+})
 })
