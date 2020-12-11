3f771fe9d8020341a36e71a4c183da65f88584bb
Choi Jiwon <winning1993@naver.com>
2020-12-11 18:11:43 +0900

    #9.1 Setup part Two

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
+import { getConnection } from 'typeorm';
 
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
@@ -15,11 +16,16 @@ describe('UserModule (e2e)', () => {
     await app.init();
   });
 
+  afterAll(async () => {
+    await getConnection().dropDatabase()
+    app.close()
+  })
+
+  it.todo("createAccount")
+  it.todo("userProfile")
+  it.todo("login")
 it.todo("me")
-it.todo("userProfile")
-it.todo("createAccount")
-it.todo("login")
-it.todo("editProfile")
 it.todo("verifyEmail")
+it.todo("editProfile")
 
 });
