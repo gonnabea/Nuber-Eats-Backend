d32407835e65c8d1a67624335b4cc6d7bdf57c78
Choi Jiwon <winning1993@naver.com>
2020-12-17 19:58:51 +0900

    #10.11 Delete Restaurant

--- src/restaurants/restaurants.service.ts
+++ src/restaurants/restaurants.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from "@nestjs/typeorm";
 import { User } from "src/users/entities/user.entity";
 import { Repository } from "typeorm";
 import { CreateRestaurantInput, CreateRestaurantOutput } from "./dtos/create-restaurant.dto";
+import { DeleteRestaurantInput, DeleteRestaurantOutput } from "./dtos/delete-restaurant.dto";
 import { EditRestaurantInput, EditRestaurantOutput } from "./dtos/edit-restaurant.dto";
 import { Category } from "./entities/category.entity";
 import { Restaurant } from "./entities/restaurant.entity";
@@ -74,4 +75,36 @@ export class RestaurantService {
             }
         }
     }
+
+    async deleteRestaurant(owner: User, {restaurantId}: DeleteRestaurantInput
+    ): Promise<DeleteRestaurantOutput> {
+        try{
+            const restaurant = await this.restaurants.findOne(restaurantId)
+    
+            if(!restaurant){
+                return {
+                    ok: false,
+                    error: "Restaurant not found"
+                }
+            }
+            if(owner.id !== restaurant.ownerId) {
+                return {
+                    ok: false,
+                    error: "You can't edit a restaurant that you don't own"
+                }
+            }
+
+            await this.restaurants.delete(restaurantId)
+
+            return {
+                ok: true
+            }
+        }catch(error){
+            return {
+                ok: false,
+                error: "Could not delete restaurant."
+            }
+        }
+        }
+    
 }
