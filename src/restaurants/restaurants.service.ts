a8ce48da82c02edf7dd01cfb8dcac446fa0e9229
Choi Jiwon <winning1993@naver.com>
2020-12-18 09:47:44 +0900

    #10.14 Category

--- src/restaurants/restaurants.service.ts
+++ src/restaurants/restaurants.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from "@nestjs/typeorm";
 import { User } from "src/users/entities/user.entity";
 import { Repository } from "typeorm";
 import { AllCategoriesOutput } from "./dtos/all-categirues,dto";
+import { CategoryInput, CategoryOutput } from "./dtos/category.dto";
 import { CreateRestaurantInput, CreateRestaurantOutput } from "./dtos/create-restaurant.dto";
 import { DeleteRestaurantInput, DeleteRestaurantOutput } from "./dtos/delete-restaurant.dto";
 import { EditRestaurantInput, EditRestaurantOutput } from "./dtos/edit-restaurant.dto";
@@ -125,5 +126,26 @@ export class RestaurantService {
 
         countRestaurant(category:Category) {
             return this.restaurants.count({category})
+        }
+
+        async findCategoryBySlug({slug}:CategoryInput): Promise<CategoryOutput> {
+            try{
+                const category = await this.categories.findOne({slug}, {relations:["restaurants"]})
+                if(!category) {
+                    return {
+                        ok: false,
+                        error: "Category not found"
+                    }
+                }
+                return {
+                    ok: true,
+                    category
+                }
+            }catch {
+                return {
+                    ok: false,
+                    error: "Could not load category"
+                }
+            }
         }
 }
