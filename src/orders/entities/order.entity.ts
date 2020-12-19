d570bea6b7d8bc549c7a5d7082ff4717ef4c91e7
Choi Jiwon <winning1993@naver.com>
2020-12-19 22:30:10 +0900

    #11.6 Order Items

--- src/orders/entities/order.entity.ts
+++ src/orders/entities/order.entity.ts
@@ -1,9 +1,11 @@
 import { Field, Float, InputType, Int, ObjectType, registerEnumType } from "@nestjs/graphql";
+import { IsEnum, IsNumber } from "class-validator";
 import { CoreEntity } from "src/common/entities/core.entity";
 import { Dish } from "src/restaurants/entities/dish.entity";
 import { Restaurant } from "src/restaurants/entities/restaurant.entity";
 import { User } from "src/users/entities/user.entity";
 import { Column, Entity, JoinTable, ManyToMany, ManyToOne } from "typeorm";
+import { OrderItem } from "./order-item.entity";
 
 enum OrderStatus {
     Pending = "Pending",
@@ -31,16 +33,18 @@ export class Order extends CoreEntity {
     @ManyToOne(type => Restaurant, Restaurant => Restaurant.orders, {onDelete: 'SET NULL', nullable: true})
     restaurant: Restaurant
 
-    @Field(type => [Dish])
-    @ManyToMany( type => Dish)
+    @Field(type => [OrderItem])
+    @ManyToMany( type => OrderItem)
     @JoinTable()
-    dishes: Dish[]
+    items: OrderItem[]
 
     @Column({nullable: true})
     @Field(type => Float, {nullable: true})
+    @IsNumber()
     total?: number // 총 결제금액
 
     @Column({type:"enum", enum: OrderStatus})
     @Field(type => OrderStatus)
+    @IsEnum(OrderStatus)
     status: OrderStatus
 }
