822db60af1a88b1461751fbb38899788b3d8e877
Choi Jiwon <winning1993@naver.com>
2020-12-20 10:30:57 +0900

    #11.10 Create Order part Five

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -29,21 +29,52 @@ export class OrderService {
                 error: "Restaurant not found"
             }
         }
-        items.forEach(async item => {
+        for (const item of items){
+
+            // dish: 데이터베이스에 저장되어 있는 음식 목록
+            // item: 유저가 요청한 음식
             const dish = await this.dishes.findOne(item.dishId)
+
             if(!dish) {
-                // abort this whole thing
+               return {
+                   ok: false,
+                   error: "Dish not found."
+               }
             }
-            await this.OrderItems.save(this.OrderItems.create({
-                dish,
-                options: item.options
-            }))
-        })
-        const order = await this.orders.save(this.orders.create({
-            customer,
-            restaurant
-        }))
-        console.log(order)
+
+            for(const itemOption of item.options) {
+                const dishOption = dish.options.find(dishOption => {
+                    dishOption.name === itemOption.name
+                })
+                if(dishOption) {
+                    if(dishOption.extra) {
+                        // 옵션에 추가 비용이 O ex) 핫소스 추가: 200원
+
+                    } 
+                    else {
+                        // 옵션에 추가 비용이 X
+                        const dishOptionChoice = dishOption.choices.find(optionChoice => optionChoice.name === itemOption.choice )
+                        if(dishOptionChoice) {
+                            if(dishOptionChoice.extra) {
+                                // 옵션의 초이스에 추가 비용이 O  ex) 피자의 사이즈 중 M, L XL 등등
+
+                            }
+                            // 옵션의 초이스에도 추가 비용이 X ex) 기본맛, 매운맛, 아주 매운맛 등으로 변경
+                            
+                        }
+                    }
+                }
+            }
+            // await this.OrderItems.save(this.OrderItems.create({
+            //     dish,
+            //     options: item.options
+            // }))
+        }
+        // const order = await this.orders.save(this.orders.create({
+        //     customer,
+        //     restaurant
+        // }))
+        // console.log(order)
     }
 
 }
