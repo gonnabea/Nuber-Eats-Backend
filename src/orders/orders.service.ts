344c2296c1b3e10efb5485d9b777c14b957538f9
Choi Jiwon <winning1993@naver.com>
2020-12-20 12:56:41 +0900

    #11.11 Create Order part Six

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -5,7 +5,7 @@ import { Restaurant } from "src/restaurants/entities/restaurant.entity";
 import { User } from "src/users/entities/user.entity";
 import { Repository } from "typeorm";
 import { CreateOrderInput, CreateOrderOutput } from "./dtos/create-order.dto";
-import { OrderItem } from "./entities/order-item.entity";
+import { OrderItem, OrderItemOption } from "./entities/order-item.entity";
 import { Order } from "./entities/order.entity";
 
 @Injectable()
@@ -22,59 +22,78 @@ export class OrderService {
     ) {}
 
     async createOrder(customer: User, {restaurantId,items}:CreateOrderInput): Promise<CreateOrderOutput> {
-        const restaurant = await this.restaurant.findOne(restaurantId)
-        if(!restaurant){
-            return {
-                ok: false,
-                error: "Restaurant not found"
+
+        try{
+            const restaurant = await this.restaurant.findOne(restaurantId)
+            if(!restaurant){
+                return {
+                    ok: false,
+                    error: "Restaurant not found"
+                }
             }
-        }
-        for (const item of items){
-
-            // dish: 데이터베이스에 저장되어 있는 음식 목록
-            // item: 유저가 요청한 음식
-            const dish = await this.dishes.findOne(item.dishId)
-
-            if(!dish) {
-               return {
-                   ok: false,
-                   error: "Dish not found."
-               }
-            }
-
-            for(const itemOption of item.options) {
-                const dishOption = dish.options.find(dishOption => {
-                    dishOption.name === itemOption.name
-                })
-                if(dishOption) {
-                    if(dishOption.extra) {
-                        // 옵션에 추가 비용이 O ex) 핫소스 추가: 200원
-
-                    } 
-                    else {
-                        // 옵션에 추가 비용이 X
-                        const dishOptionChoice = dishOption.choices.find(optionChoice => optionChoice.name === itemOption.choice )
-                        if(dishOptionChoice) {
-                            if(dishOptionChoice.extra) {
-                                // 옵션의 초이스에 추가 비용이 O  ex) 피자의 사이즈 중 M, L XL 등등
-
+            let orderFinalPrice = 0
+            const orderItems: OrderItem[] = []
+            for (const item of items){
+    
+                // dish: 데이터베이스에 저장되어 있는 음식 목록
+                // item: 유저가 요청한 음식
+                const dish = await this.dishes.findOne(item.dishId)
+    
+                if(!dish) {
+                   return {
+                       ok: false,
+                       error: "Dish not found."
+                   }
+                }
+                let dishFinalPrice = dish.price;
+    
+                for(const itemOption of item.options) {
+                    const dishOption = dish.options.find(dishOption => {
+                        dishOption.name === itemOption.name
+                    })
+                    if(dishOption) {
+                        if(dishOption.extra) {
+                            // 옵션에 추가 비용이 O ex) 핫소스 추가: 200원
+                            dishFinalPrice = dishFinalPrice + dishOption.extra
+                        } 
+                        else {
+                            // 옵션에 추가 비용이 X
+                            const dishOptionChoice = dishOption.choices.find(optionChoice => optionChoice.name === itemOption.choice )
+                            if(dishOptionChoice) {
+                                if(dishOptionChoice.extra) {
+                                    // 옵션의 초이스에 추가 비용이 O  ex) 피자의 사이즈 중 M, L XL 등등
+                            dishFinalPrice = dishFinalPrice + dishOptionChoice.extra
+                                }
+                                // 옵션의 초이스에도 추가 비용이 X ex) 기본맛, 매운맛, 아주 매운맛 등으로 변경
+                                
                             }
-                            // 옵션의 초이스에도 추가 비용이 X ex) 기본맛, 매운맛, 아주 매운맛 등으로 변경
-                            
                         }
                     }
                 }
+                orderFinalPrice = orderFinalPrice + dishFinalPrice
+                const orderItem = await this.OrderItems.save(this.OrderItems.create({
+                    dish,
+                    options: item.options
+                }))
+                orderItems.push(orderItem)
             }
-            // await this.OrderItems.save(this.OrderItems.create({
-            //     dish,
-            //     options: item.options
-            // }))
+            const order = await this.orders.save(this.orders.create({
+                customer,
+                restaurant,
+                total: orderFinalPrice,
+                items: orderItems
+            }))
+            return {
+                ok: true
+            }
+        }catch {
+            return {
+                ok: false,
+                error: "Could not create order"
+            }
         }
-        // const order = await this.orders.save(this.orders.create({
-        //     customer,
-        //     restaurant
-        // }))
-        // console.log(order)
     }
 
 }
+
+// 과제: 테스팅 만들어보기 #11.11
