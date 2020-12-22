dc80a6bc2cf8c130cabcd6142d5394473de8cc5a
Choi Jiwon <winning1993@naver.com>
2020-12-22 13:34:28 +0900

    #13.3 createPayment part Two

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
 import { Payment } from './entities/payment.entity';
 import { PaymentResolver } from './entities/payments.resolver';
 import { PaymentService } from './entities/payments.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Payment])],
+  imports: [TypeOrmModule.forFeature([Payment, Restaurant])],
   providers: [PaymentService, PaymentResolver],
 })
 export class PaymentsModule {}
