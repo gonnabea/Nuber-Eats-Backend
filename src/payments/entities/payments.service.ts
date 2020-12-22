3d395a97e599cff19db7fdb46c7b515c2f11db9b
Choi Jiwon <winning1993@naver.com>
2020-12-22 15:20:38 +0900

    #13.4 getPayments Resolver

--- src/payments/entities/payments.service.ts
+++ src/payments/entities/payments.service.ts
@@ -7,6 +7,7 @@ import {
   CreatePaymentInput,
   CreatePaymentOutput,
 } from '../dtos/create-payment.dto';
+import { GetPaymentsOutput } from '../dtos/get-payments.dto';
 import { Payment } from './payment.entity';
 
 @Injectable()
@@ -50,6 +51,21 @@ export class PaymentService {
       return {
         ok: false,
         error: 'Could not create payment.',
+      };
+    }
+  }
+
+  async getPayments(user: User): Promise<GetPaymentsOutput> {
+    try {
+      const payments = await this.payments.find({ user: user });
+      return {
+        ok: true,
+        payments,
+      };
+    } catch {
+      return {
+        ok: false,
+        error: 'Could not load payments.',
       };
     }
   }
